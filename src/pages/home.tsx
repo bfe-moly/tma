6aa249b782d23a4a8cadac47a1538583fc398589
fanyong <fanyong@gmail.com>
2024-08-15 22:09:33 +0800

    feat: debug

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,20 @@ import WebApp from '@twa-dev/sdk';
 import { MainButton } from '@twa-dev/sdk/react';
 
 const Home = () => {
+  const shareToStory = () => {
+    if (WebApp) {
+      WebApp.shareToStory(
+        'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png',
+        {
+          text: 'text',
+          widget_link: {
+            name: 'name',
+            url: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png',
+          },
+        }
+      );
+    }
+  };
   return (
     <div>
       <h1>Home Page</h1>
@@ -19,7 +33,7 @@ const Home = () => {
         <button>授权</button>
       </div>
       <div>
-        <button>分享</button>
+        <button onClick={shareToStory}>分享</button>
       </div>
     </div>
   );
