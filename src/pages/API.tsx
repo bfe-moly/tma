9fd8dadd1a366e8b2f7408e9e6b9f69af3b577fe
fanyong <fanyong@gmail.com>
2024-08-21 19:16:37 +0800

    feat: add scafold

---
+++ src/pages/API.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Api() {
+  return (
+    <div>
+      <h2>Api</h2>
+    </div>
+  );
+}
+
+export default Api;
---
+++ src/pages/Components.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Components() {
+  return (
+    <div>
+      <h2>Components</h2>
+    </div>
+  );
+}
+
+export default Components;
