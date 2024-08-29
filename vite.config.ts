2ae96c1941aeb020a998fd0f5690e22952b8d049
dm <dm@gmail.com>
2024-08-29 16:17:40 +0800

    feat: add

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 import fs from 'fs';
 import path from 'path';
+let key = ''
+let cert = ''
+
+try {
+  key = fs.readFileSync(path.resolve(__dirname, 'local.tg.bybit.com-key.pem')),
+      cert= fs.readFileSync(path.resolve(__dirname, 'local.tg.bybit.com.pem')),
+} catch (error) {
+  console.error('key 和 cert 不存在')
+}
 
 export default defineConfig({
   base: '/tma/',
@@ -8,10 +17,8 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     https: {
-      key: fs.readFileSync(
-        path.resolve(__dirname, 'local.tg.bybit.com-key.pem'),
-      ),
-      cert: fs.readFileSync(path.resolve(__dirname, 'local.tg.bybit.com.pem')),
+      key,
+      cert,
     },
   },
   plugins: [
