5a949ac4ce290e4b22fac7494fd4dedd3bab23ee
dm <dm@gmail.com>
2024-08-30 14:23:32 +0800

    fix: 完成 分享

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,21 +1,31 @@
 // import { postEvent } from "@telegram-apps/sdk";
 import { Button } from 'antd-mobile';
-import React, { useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 
 import WebApp from '@twa-dev/sdk';
 
-export default (props: any) => {
+interface ShareProps {
+  index?: number;
+}
+
+const Share: FC<ShareProps> =  (props) => {
   const [isSharing, setIsSharing] = useState(false);
+  useEffect(() => {
+
+  }, [])
+  
 
   const handleShare = async () => {
     setIsSharing(true);
     try {
       // WebApp.showAlert("Hey there!");
       try {
-        const shareText =
-          'Check out this awesome Telegram Mini App: [Your Mini App Link Here]';
-        // await WebApp.shareToStory(shareText);
-        WebApp.switchInlineQuery('', ['users', 'groups', 'channels']);
+        // const shareText =
+        //   'Check out this awesome Telegram Mini App: [Your Mini App Link Here]';
+        // // await WebApp.shareToStory(shareText);
+        // WebApp.switchInlineQuery('', ['users', 'groups', 'channels']);
+        const link = "https://t.me/BybitDevBot/devweb?startapp=NIWx3ui_QiadeSWoiRiSKg";
+        WebApp.openTelegramLink(`https://t.me/share/url?url=${link}&text=Dream Ha Ha!`)
         console.log('分享成功');
       } catch (error) {
         console.error('分享失败:', error);
@@ -36,7 +46,9 @@ export default (props: any) => {
   return (
     <Button color="success" onClick={handleShare}>
       {/* <Button onClick={test1}> */}
-      {isSharing ? '分享中...' : '分享到 Telegram 用户、群组或频道'}
+      {isSharing ? '分享中...' : '分享到 Telegram 用户、群组或频道-v1'}
     </Button>
   );
 };
+
+export default Share
